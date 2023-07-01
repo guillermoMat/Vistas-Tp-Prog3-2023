0ec875b12c1f1578e7a43b07fba1665a0c5d3249
guillermo <guillermomathieu@hotmail.com>
2023-07-01 19:03:48 -0300

    1er subida-login con vistas

---
+++ TpFinalWeb/Controllers/AccesoController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+
+using TpFinalWeb.Models;
+
+namespace TpFinalWeb.Controllers
+{
+    public class AccesoController : Controller
+    {
+        static string cadenaConexion = "Data Source=DESKTOP-3D9QJNG;Initial Catalog=Usuario;Integrated Security=true";
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Usuario oUsuario)
+        {
+            using (SqlConnection cn=new SqlConnection(cadenaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("sp_ValidarUsuario",cn);
+                cmd.Parameters.AddWithValue("Correo",oUsuario.Correo);
+                cmd.Parameters.AddWithValue("Clave",oUsuario.Clave);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+
+                oUsuario.Id=Convert.ToInt32(cmd.ExecuteScalar().ToString());//solamente nos lee la primera fila y primer columna
+            }
+
+            if (oUsuario.Id!=0)
+            {
+                ViewData["usuario"] = oUsuario;
+
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ViewData["Mensaje"] = "Usuario no encontrado";
+                return View();
+            }
+
+        }
+
+        
+    }
+}
---
+++ TpFinalWeb/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+//using System.Web.Mvc;
+
+using TpFinalWeb.Models;
+
+
+namespace TpFinalWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult CerrarSesion()
+        {
+            ViewData["usuario"] = null;
+
+            return RedirectToAction("Login", "Acceso");
+        }
+        public IActionResult BandejaEntrada()
+        {
+            return View();
+        }
+        public IActionResult ElementosEnviados()
+        {
+            return View();
+        }
+        public IActionResult EnviarCorreo()
+        {
+            return View();
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
